40d4b57c8a30f14923dfcfadcef13a7e214f02f8
tronxi <sergio.gsanchez97@gmail.com>
2022-03-22 13:42:11 +0100

    mark video as completed

--- src/app/lesson-video/lesson-video.component.ts
+++ src/app/lesson-video/lesson-video.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {LessonService} from "../services/lesson.service";
 import {DomSanitizer, SafeResourceUrl, SafeUrl} from "@angular/platform-browser";
+import {NgbModal, NgbModalConfig} from "@ng-bootstrap/ng-bootstrap";
+import {TokenService} from "../services/token.service";
 
 @Component({
   selector: 'app-lesson-video',
@@ -13,11 +15,18 @@ export class LessonVideoComponent implements OnInit {
   lessonId : string  = "1";
   lesson = {title: '', text: ''};
   url: SafeResourceUrl | undefined;
+  @ViewChild("modal") modal: ElementRef = {} as ElementRef;
 
   constructor(private route: ActivatedRoute,
               private router : Router,
               private lessonService: LessonService,
-              public sanitizer: DomSanitizer) { }
+              public sanitizer: DomSanitizer,
+              config: NgbModalConfig,
+              private modalService: NgbModal,
+              private tokenService: TokenService) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   ngOnInit(): void {
     this.lessonId = this.route.snapshot.paramMap.get('lessonId') || "1";
@@ -30,4 +39,14 @@ export class LessonVideoComponent implements OnInit {
     });
   }
 
+  onEnded() {
+    this.modalService.open(this.modal);
+  }
+
+  onClose() {
+    this.modalService.dismissAll();
+    this.lessonService.markAsCompleted(this.tokenService.retrieveUserId(), this.lessonId).subscribe({
+      next: value => this.router.navigateByUrl('practice')
+    })
+  }
 }
